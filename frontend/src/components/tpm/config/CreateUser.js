8be9b3ef24f038910a7e35305bf8234aca83d5ee
oscarmg11 <os.kardavid70@gmail.com>
2021-04-05 11:08:18 -0500

    update delete user

--- frontend/src/components/tpm/config/CreateUser.js
+++ frontend/src/components/tpm/config/CreateUser.js
@@ -94,10 +94,7 @@ function CreateUser({ modalOpen, closeModal, userToEdit, addUser }){
             }else{
                 postData({data: JSON.stringify({ user, password, tipoUsuario: typeUser, linea: line, email })})
                 .then(({ usuario }) => {
-                    console.log(usuario)
-                    console.log(JSON.parse(usuario))
-                    const user = JSON.parse(usuario)
-                    addUser({ id: user.id, email: user.email, linea: user.linea, username: user.username })
+                    addUser({ id: usuario.id, email: usuario.email, linea: usuario.linea, username: usuario.username })
                     closeModal()    
                 }).catch(e => console.log(e))
             }
