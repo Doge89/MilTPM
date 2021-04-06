c5e26664690a8ede4332ed944e83a3331aadff5d
oscarmg11 <os.kardavid70@gmail.com>
2021-04-06 12:17:10 -0500

    update tpm get id tarjeta conforme/no conforme

--- frontend/src/components/tpm/history/HistorySearch.js
+++ frontend/src/components/tpm/history/HistorySearch.js
@@ -1,13 +1,34 @@
 import React, { useState } from 'react'
+import axios from 'axios'
+import querystring from 'querystring'
 
 import { HistorySearchContainer } from '../../../styles/tpm'
 import { ButtonSecondary } from '../../../styles/common'
 
-function HistorySearch(){
+import { URL } from '../../../var'
+
+function HistorySearch({ setHistory }){
 
     const [id, setId] = useState('')
 
+    const getCard = async () => {
+        const res = await axios({
+            url: `${URL}/tpm/historial/get/id/`,
+            method: 'POST',
+            data: querystring.stringify({ id: id })
+        })
+
+        return res.data
+    }
+
     const handleInput = e => setId(e.target.value)
+
+    const handleBtn = () => {
+        getCard().then(({ card }) => {
+            console.log(card)
+            setHistory([card])
+        }).catch(e => console.log(e))
+    }
 
     return(
         <HistorySearchContainer>
@@ -17,7 +38,7 @@ function HistorySearch(){
                 placeholder="Folio"
                 type="number"
             />
-            <ButtonSecondary height="4vh" width="8vw" className="size-effect">Buscar</ButtonSecondary>
+            <ButtonSecondary height="4vh" width="8vw" className="size-effect" onClick={handleBtn}>Buscar</ButtonSecondary>
         </HistorySearchContainer>
     )
 }
