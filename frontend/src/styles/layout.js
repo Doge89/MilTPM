4469fd9aee304d6ba0f57f21e6a86570cdf90734
oscarmg11 <os.kardavid70@gmail.com>
2021-04-19 15:28:35 -0500

    update mp admin

--- frontend/src/styles/layout.js
+++ frontend/src/styles/layout.js
@@ -32,8 +32,8 @@ export const LayoutContainer = styled.div`
 `;
 
 export const Plane = styled.div`
-    width: 80%;
-    height: 85%;
+    width: 75%;
+    height: 80%;
     background-image: ${props => `url(${props.img})`};
     background-repeat: no-repeat;
     background-size: 100% 100%;
